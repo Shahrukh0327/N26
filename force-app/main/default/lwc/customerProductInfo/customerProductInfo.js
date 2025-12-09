8305c3f2693e2375a4dba968fdcf4d07bf68441b
Shahrukh Ahmed <Shahrukhahmed0327@gmail.com>
2025-12-09 12:30:47 +0100

    further changes

--- force-app/main/default/lwc/customerProductInfo/customerProductInfo.js
+++ force-app/main/default/lwc/customerProductInfo/customerProductInfo.js
@@ -10,12 +10,16 @@ export default class CustomerProductInfo extends LightningElement {
 	@api recordId;
 	productURL;
 	productInfo;
+
+	//	 Fetch case details to get ContactId
 	@wire(getRecord, { recordId: '$recordId', fields: [CONTACT_ID_FIELD] })
 	case;
 
 	get contactId() {
 		return getFieldValue(this.case.data, CONTACT_ID_FIELD);
 	}
+
+	//	 Fetch contact details	
 	@wire(getRecord, { recordId: '$contactId', fields: [HOME_COUNTRY_FIELD, PRODUCT_ID_FIELD] })
 	contact;
 
@@ -26,22 +30,28 @@ export default class CustomerProductInfo extends LightningElement {
 		return getFieldValue(this.contact.data, PRODUCT_ID_FIELD);
 	}
 
+	//	 Fetch product pricing information
 	@wire(getProductPricing, { productId: '$productId', homeCountry: '$homeCountry' })
 	productPricing({ error, data }) {
 		if (data) {
-			
-			this.productURL= '/'+data.productId;
+			//	 Creating product URL
+			this.productURL = '/' + this.productId;
 			this.productInfo = data[0];
+
 			console.log('Product Pricing Data:', this.productInfo.product);
+
 		} else if (error) {
-			let errorMessage;
+
+			let errorMessage = 'An error occurred while fetching product pricing information.';
+
 			if (error.body.message) {
 				errorMessage = error.body.message;
 			}
+
 			this.showToast("Failure", errorMessage, "error");
 		}
 	}
-
+	// Function to show toast messages
 	showToast(title, message, variant) {
 		const event = new ShowToastEvent({ title, message, variant });
 		this.dispatchEvent(event);
